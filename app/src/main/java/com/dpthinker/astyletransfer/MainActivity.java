925c0796287bfbb8a0bc625b51ce9d7063aae357
Jinpeng Zhu <dpinthinker@gmail.com>
2020-01-05 21:59:08 +0800

    make first running, but still seems style is not good enough

--- app/src/main/java/com/dpthinker/astyletransfer/MainActivity.java
+++ app/src/main/java/com/dpthinker/astyletransfer/MainActivity.java
@@ -9,17 +9,19 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import org.tensorflow.lite.DataType;
 import org.tensorflow.lite.Interpreter;
 import org.tensorflow.lite.gpu.GpuDelegate;
 import org.tensorflow.lite.nnapi.NnApiDelegate;
+import org.tensorflow.lite.support.common.ops.NormalizeOp;
+import org.tensorflow.lite.support.image.ImageProcessor;
 import org.tensorflow.lite.support.image.TensorImage;
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
@@ -29,7 +31,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.HashMap;
@@ -44,7 +45,6 @@ public class MainActivity extends AppCompatActivity {
 
     private Bitmap mContentImage;
     private Bitmap mStyleImage;
-    private Bitmap mTransferredImage;
 
     private RadioGroup mRadioGroup;
     private Button mTransferButton;
@@ -54,7 +54,6 @@ public class MainActivity extends AppCompatActivity {
 
     private final static int STYLE_IMG_SIZE = 256;
     private final static int CONTENT_IMG_SIZE = 384;
-    private final static int NUM_BYTES_PER_CHANNEL = 4;
     private final static int DIM_BATCH_SIZE = 1;
     private final static int DIM_PIXEL_SIZE = 3;
 
@@ -62,10 +61,13 @@ public class MainActivity extends AppCompatActivity {
     private final static int USING_GPU = 2;
     private final static int USING_NNAPI = 3;
 
-    private int mDelagationMode = USING_CPU;
+    private int mDelegationMode = USING_CPU;
 
     private final static String PREDICT_MODEL = "style_predict.tflite";
-    private final static String TRANSFROM_MODE = "style_transform.tflite";
+    private final static String TRANSFORM_MODE = "style_transform.tflite";
+
+    private final static int IMAGE_MEAN = 0;
+    private final static int IMAGE_STD = 255;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initViews() {
+        // init content and style imageview
         mContentImageView = findViewById(R.id.content_img);
         mContentImageView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -102,27 +105,36 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        // init radio group
         mRadioGroup = findViewById(R.id.rg_model);
         mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
                 if (i == R.id.rb_cpu) {
-                    mDelagationMode = USING_CPU;
+                    mDelegationMode = USING_CPU;
                 } else if (i == R.id.rb_gpu) {
-                    mDelagationMode = USING_GPU;
+                    mDelegationMode = USING_GPU;
                 } else if (i == R.id.rb_nnapi) {
-                    mDelagationMode = USING_NNAPI;
+                    mDelegationMode = USING_NNAPI;
                 }
             }
         });
 
+        // init transfer button
         mTransferButton = findViewById(R.id.bt_transfer);
         mTransferButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (mContentImage == null) {
+                    Toast.makeText(MainActivity.this, R.string.no_contentimg, Toast. LENGTH_SHORT).show();
+                    return;
+                }
+                if (mStyleImage == null) {
+                    Toast.makeText(MainActivity.this, R.string.no_styleimg, Toast. LENGTH_SHORT).show();
+                    return;
+                }
                 Interpreter.Options options = new Interpreter.Options();
-                switch (mDelagationMode) {
+                switch (mDelegationMode) {
                     case USING_CPU:
                         break;
                     case USING_GPU:
@@ -134,16 +146,16 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 try {
-                    // Should init two interpreter instances: style predict and style transform
+                    // init two interpreter instances: style predict and style transform
                     Interpreter predictInterpreter = new Interpreter(
                             loadModelFile(MainActivity.this, PREDICT_MODEL), options);
 
-                    Interpreter tranfromInterpreter = new Interpreter(
-                            loadModelFile(MainActivity.this, TRANSFROM_MODE), options);
+                    Interpreter transformInterpreter = new Interpreter(
+                            loadModelFile(MainActivity.this, TRANSFORM_MODE), options);
 
                     ByteBuffer bottleneck = runPredict(predictInterpreter, mStyleImage);
 
-                    Bitmap mTransferredImage = runTransform(tranfromInterpreter, mContentImage, bottleneck);
+                    Bitmap mTransferredImage = runTransform(transformInterpreter, mContentImage, bottleneck);
                     saveImage(mTransferredImage);
 
                     Intent intent = new Intent(MainActivity.this, TransferredActivity.class);
@@ -159,10 +171,10 @@ public class MainActivity extends AppCompatActivity {
         String dataDir = getFilesDir().toString();
         File myDir = new File(dataDir);
         myDir.mkdirs();
-        String fname = "TransferredImage.jpg";
-        File file = new File(myDir, fname);
+        String imageName = "TransferredImage.jpg";
+        File file = new File(myDir, imageName);
         if (file.exists()) file.delete();
-        Log.i(TAG, dataDir + "/"+ fname);
+        Log.i(TAG, "save transferred image to " + dataDir + "/"+ imageName);
         try {
             FileOutputStream out = new FileOutputStream(file);
             finalBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
@@ -175,80 +187,75 @@ public class MainActivity extends AppCompatActivity {
     private ByteBuffer runPredict(Interpreter tflite, Bitmap styleImage) {
         int imageTensorIndex = 0;
         DataType imageDataType = tflite.getInputTensor(imageTensorIndex).dataType();
-        TensorImage inputImageBuffer = new TensorImage(imageDataType);
-        inputImageBuffer.load(styleImage);
-        inputImageBuffer.getBuffer().rewind();
+        TensorImage inputTensorImage = new TensorImage(imageDataType);
+        inputTensorImage.load(styleImage);
 
-        int probabilityTensorIndex = 0;
-        int[] probabilityShape =
-                tflite.getOutputTensor(probabilityTensorIndex).shape();
-        DataType probabilityDataType = tflite.getOutputTensor(probabilityTensorIndex).dataType();
-        TensorBuffer outputProbabilityBuffer
-                = TensorBuffer.createFixedSize(probabilityShape, probabilityDataType);
+        ImageProcessor imageProcessor =
+                new ImageProcessor.Builder()
+                        .add(new NormalizeOp(IMAGE_MEAN, IMAGE_STD))
+                        .build();
+        inputTensorImage = imageProcessor.process(inputTensorImage);
 
-        tflite.run(inputImageBuffer.getBuffer(), outputProbabilityBuffer.getBuffer().rewind());
+        int outputTensorIndex = 0;
+        int[] outputShape = tflite.getOutputTensor(outputTensorIndex).shape();
+        DataType outputDataType = tflite.getOutputTensor(outputTensorIndex).dataType();
+        TensorBuffer outputTensorBuffer
+                = TensorBuffer.createFixedSize(outputShape, outputDataType);
 
-        float[] out = outputProbabilityBuffer.getFloatArray();
-//        for (float f : out ) {
-//            Log.e(TAG, "out: " + f);
-//        }
+        tflite.run(inputTensorImage.getBuffer(), outputTensorBuffer.getBuffer().rewind());
 
-        Log.e(TAG, "out size: " + out.length);
-        outputProbabilityBuffer.getBuffer().rewind();
+        outputTensorBuffer.getBuffer().rewind();
 
-
-
-        return outputProbabilityBuffer.getBuffer();
+        return outputTensorBuffer.getBuffer();
     }
 
     private Bitmap runTransform(Interpreter tflite, Bitmap contentImage, ByteBuffer bottleneck) {
         int imageTensorIndex = 0;
         DataType imageDataType = tflite.getInputTensor(imageTensorIndex).dataType();
-        TensorImage inputImageBuffer = new TensorImage(imageDataType);
-        inputImageBuffer.load(contentImage);
-        inputImageBuffer.getBuffer().rewind();
+        TensorImage inputTensorImage = new TensorImage(imageDataType);
+        inputTensorImage.load(contentImage);
+
+        ImageProcessor imageProcessor =
+                new ImageProcessor.Builder()
+                        .add(new NormalizeOp(IMAGE_MEAN, IMAGE_STD))
+                        .build();
+        inputTensorImage = imageProcessor.process(inputTensorImage);
+
+        inputTensorImage.getBuffer().rewind();
 
         Object[] inputs = new Object[2];
-        inputs[0] = inputImageBuffer.getBuffer();
+        inputs[0] = inputTensorImage.getBuffer();
         inputs[1] = bottleneck;
 
-        int probabilityTensorIndex = 0;
-        int[] probabilityShape = new int[]{
+        int outputTensorIndex = 0;
+        int[] outputShape = new int[]{
                 DIM_BATCH_SIZE, CONTENT_IMG_SIZE, CONTENT_IMG_SIZE, DIM_PIXEL_SIZE};
-        DataType probabilityDataType = tflite.getOutputTensor(probabilityTensorIndex).dataType();
-        TensorBuffer outputProbabilityBuffer
-                = TensorBuffer.createFixedSize(probabilityShape, probabilityDataType);
+        DataType outputDataType = tflite.getOutputTensor(outputTensorIndex).dataType();
+        TensorBuffer outputTensorBuffer
+                = TensorBuffer.createFixedSize(outputShape, outputDataType);
 
         Map<Integer, Object> outputs = new HashMap<>();
-        outputs.put(0, outputProbabilityBuffer.getBuffer());
+        outputs.put(0, outputTensorBuffer.getBuffer().rewind());
         tflite.runForMultipleInputsOutputs(inputs, outputs);
-        outputProbabilityBuffer.getBuffer().rewind();
-        float[] outFloatArray = outputProbabilityBuffer.getFloatArray();
-        int[] shape = outputProbabilityBuffer.getShape();
-        for (int s: shape) {
-            Log.e(TAG, "outputProbabilityBuffer.getShape() item : " + s);
-        }
-        Log.e(TAG,"outFloatArray size: " + outFloatArray.length);
-//        for (float i : outFloatArray) {
-//            Log.e(TAG, "outFloatArray item: " + i);
+        outputTensorBuffer.getBuffer().rewind();
+        float[] outputFloatArray = outputTensorBuffer.getFloatArray();
+//        for (float f : outputFloatArray) {
+//            Log.e(TAG, "runTransform outputFloatArray item: " + f);
 //        }
-        return convertOutputToBmp(outFloatArray);
+        return convertOutputToBmp(outputFloatArray);
+
     }
 
-    private Bitmap convertOutputToBmp(float[] out) {
+    private Bitmap convertOutputToBmp(float[] output) {
         Bitmap result = Bitmap.createBitmap(
-                CONTENT_IMG_SIZE , CONTENT_IMG_SIZE,Bitmap.Config.ARGB_8888);
+                CONTENT_IMG_SIZE, CONTENT_IMG_SIZE, Bitmap.Config.ARGB_8888);
         int[] pixels = new int[CONTENT_IMG_SIZE * CONTENT_IMG_SIZE];
         for (int i = 0; i < CONTENT_IMG_SIZE * CONTENT_IMG_SIZE * 3; i++) {
             int a = 0xFF;
-            float r = out[i] * 255.0f;
-            float g = out[i++] * 255.0f;
-            float b = out[i++] * 255.0f;
-//            Log.e(TAG, "convertOutputToBmp r: " + r);
-//            Log.e(TAG, "convertOutputToBmp g: " + g);
-//            Log.e(TAG, "convertOutputToBmp b: " + b);
-            pixels[(i + 1) / 3 - 1] = a << 24 | (int)r << 16 | (int)g << 8 | (int)b;
-            //Log.e(TAG, "convertOutputToBmp : " + pixels[(i + 1) / 3 - 1]);
+            float r = output[i] * 255.0f;
+            float g = output[i++] * 255.0f;
+            float b = output[i++] * 255.0f;
+            pixels[(i + 1) / 3 - 1] = a << 24 | (int)r << 16 | (int) g << 8 | (int) b;
         }
         result.setPixels(pixels, 0, CONTENT_IMG_SIZE, 0, 0, CONTENT_IMG_SIZE, CONTENT_IMG_SIZE);
         return result;
@@ -272,8 +279,8 @@ public class MainActivity extends AppCompatActivity {
                 Bitmap bitmap = BitmapFactory.decodeStream(
                         this.getContentResolver().openInputStream(uri));
 
-                bitmap = preprocesBitmap(bitmap,
-                        requestCode == REQUEST_CONTENT_IMG? CONTENT_IMG_SIZE : STYLE_IMG_SIZE);
+                bitmap = preProcessBitmap(bitmap,
+                        requestCode == REQUEST_CONTENT_IMG ? CONTENT_IMG_SIZE : STYLE_IMG_SIZE);
 
                 if (requestCode == REQUEST_CONTENT_IMG) {
                     mContentImageView.setImageBitmap(bitmap);
@@ -289,7 +296,7 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
     }
 
-    private Bitmap preprocesBitmap(Bitmap bitmap, int size) {
+    private Bitmap preProcessBitmap(Bitmap bitmap, int size) {
         return Bitmap.createScaledBitmap(bitmap, size, size, false);
     }
 }
